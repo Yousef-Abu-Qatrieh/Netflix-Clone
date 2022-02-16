e4b7444458b4b98ec58a17b6c4bbbd519822ff07
yousef Abu Qatrieh <yousef_1995q@hotmail.com>
2022-02-17 00:43:21 +0200

    finished

---
+++ src/Components/Movie/Movie.js
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import { Button, Card } from 'react-bootstrap'
+import ModalMovie from '../ModalMovie/ModalMovie'
+
+function Movie({ mov }) {
+    const [cardInfo, setCardInfo] = useState({})
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => {
+        setShow(false)
+    }
+    return (
+        <>
+            <Card className='div-card'>
+                <Card.Img className='div-card-img' variant="top" src={`https://image.tmdb.org/t/p/w500/${mov.poster_path}`} />
+                <Card.Body>
+                    <Card.Title className='div-card-title'>{mov.title}</Card.Title>
+                    <Card.Text>
+                        {mov.release_date}
+
+                    </Card.Text>
+
+                    <Card.Text>
+                        {mov.overview}
+
+                    </Card.Text>
+                    <div>
+                        <Button className='div-card-button' variant="primary"
+                            onClick={() => {
+                                setCardInfo(mov)
+                                setShow(true);
+                            }}>Add To Favorite</Button>
+                    </div>
+                </Card.Body>
+            </Card>
+
+
+            {
+                <ModalMovie cardInfo={cardInfo} show={show} handleClose={handleClose} />
+            }
+
+
+
+        </>
+
+
+
+    )
+
+
+}
+export default Movie
--- src/Components/MovieList/MovieList.js
+++ src/Components/MovieList/MovieList.js
@@ -1,77 +1,30 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Button, Container, Row,Card, Col } from 'react-bootstrap';
-import ModalMovie from '../ModalMovie/ModalMovie'
-function MovieList(props) {
-    const [data, setData] = useState([]);
-    const [cardInfo, setCardInfo] = useState({});
-    const [show, setShow] = useState(false);
+import { Container, Row } from 'react-bootstrap';
+import Movie from '../Movie/Movie'
 
-    const handleClose = () => setShow(false);
+function MovieList({data}) {
 
-    // console.log(process.env.REACT_APP_BASE_URL);
-    const getAllTrending = async () => {
-        // await // here the thing that will happen (fetching from API or getting from database )
-        // return await axios.get(`${process.env.REACT_APP_BASE_URL}/recipes`)
-        return await axios.get(`https://moviesrepo.herokuapp.com/trending`)
-            .then(result => {
-                console.log(result.data);
-                return result.data;
-            }).catch((err) => {
-                console.log(err);
-            })
-    }
-
-    useEffect(() => {
-        void (async () => {
-            let data = await getAllTrending();
-            setData(data);
-        })();
-    }, []);
     return (
         <>
-         <Container className='div-container'>
+            <Container className='div-container'>
                 <Row md={3}>
                     {
-             
-                data.length && data.map((ele) => (
-        
 
-                    <Card  style={{ width: '50rem' }}>
-                        <Card.Img variant="top" src={ele.poster_path} />
-                        
-                        <Card.Body>
-                            <Card.Title>{ele.title}</Card.Title>
-                            <Card.Text>
-                                {ele.release_date}
+                        data.length && data.map((mov) => (
+                            <Movie mov={mov} />
+                        ))
 
-                            </Card.Text>
 
-                            <Card.Text>
-                                {ele.overview}
 
-                            </Card.Text>
-                            <div>
-                                <Button className='div-card-button' variant="primary"
-                                    onClick={() => {
-                                        setCardInfo(ele)
-                                        setShow(true);
-                                    }}>Add To Favorite</Button>
-                            </div>
 
-                        </Card.Body>
-                    </Card>
+                    }
+                    {
+                        !data.length && <div><h2>No Such Results, Please try again later </h2></div>
+                    }
 
-                ))
-            }
-
-            {
-                !data.length && <div><h2>No Such Results, Please try again later</h2></div>
-            }
-             {
-                <ModalMovie cardInfo={cardInfo} show={show} handleClose={handleClose} />
-            }
-              </Row>
+                    {/* {
+                        <ModalMovie cardInfo={cardInfo} show={show} handleClose={handleClose} />
+                    } */}
+                </Row>
             </Container>
         </>
 
