f7a03c71f219c5b6fcc58917002efef820146425
yousef Abu Qatrieh <yousef_1995q@hotmail.com>
2022-02-17 08:37:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route,Routes,Switch} from 'react-router-dom'
+import  Navbar  from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home'
-
+import FavList from './Components/FavList/FavList'
+// import  Navbar  from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+       <Navbar/>
       {/* <Navbar/> */}
    <Routes>
    <Route path='/' exact  element={<Home/>} />
-   {/* <Route path='/trending' exact element={</>} /> */}
+    <Route path='/favorite' exact element={<FavList/>} /> *
 
 
   </Routes>
--- src/Components/ModalMovie/ModalMovie.js
+++ src/Components/ModalMovie/ModalMovie.js
@@ -1,7 +1,23 @@
 import Modal from 'react-bootstrap/Modal';
 import {Button} from 'react-bootstrap';
+import axios from 'axios';
+import {useRef} from 'react';
 
 function Modalo({cardInfo, show, handleClose}) {
+    const commentInputRef = useRef("");
+   
+    const addToFav = async ()=>{
+        let comment = commentInputRef.current.value;
+       let fav = {title:cardInfo.title,release_date:cardInfo.release_date, overview:cardInfo.overview, poster_path:cardInfo.poster_path, comment:comment}
+      
+       await axios.post(`https://moviesrepo.herokuapp.com/addMovie`,fav)
+                  .then(()=>{
+                      console.log("Done :) ");
+                  }).catch((err)=>{
+                      console.log(err);
+                  });
+  
+   }
     return (
         <>
             <Modal show={show} onHide={handleClose} animation={false}>
@@ -13,11 +29,15 @@ function Modalo({cardInfo, show, handleClose}) {
                     <img alt="" src={`https://image.tmdb.org/t/p/w500/${cardInfo.poster_path}`} />
                     <div>
                         <label htmlFor="op">Write Your Opinion</label>
-                        <input placeholder="Write Your Opinion" type="text" id="op" />
+                        <input ref={commentInputRef} placeholder="Write Your Opinion" type="text" id="op" />
                     </div>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="primary"> Add To Favorite </Button>
+                    <Button variant="primary"
+                     onClick={()=>{
+                        addToFav();
+                        handleClose();
+                     }}> Add To Favorite </Button>
                 </Modal.Footer>
             </Modal>
         </>
